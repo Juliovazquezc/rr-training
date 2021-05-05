9fc3bb2398c8d03dace4b7ca3c3d96c9dde9cb73
Julio César Vázquez Cervantes <43428762+Juliovazquezc@users.noreply.github.com>
2021-05-04 21:07:00 -0500

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Tailwindcss name="Julio César Vázquez" position="Software Developer at Harman Querétaro"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Tailwindcss from '@/components/Tailwindcss.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Tailwindcss
+  }
+}
+</script>
